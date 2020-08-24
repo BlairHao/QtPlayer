9274f0e222b2b4b351e88445d787c572d557e01b
BlairHao <1325512053@qq.com>
2020-08-24 16:59:34 +0800

    archieve GLWidget scaled center on mouse

--- QtPlayer.cpp
+++ QtPlayer.cpp
@@ -12,10 +12,15 @@ QtPlayer::QtPlayer(QWidget *parent)
 {
 	ui.setupUi(this);
 
+	mbIsWheelScale = false;
+	mnScaleValue = 1.0;
+	mnPaperWidth = 800;
+	mnPaperHeight = 450;
 	//openFile();
 	connect(ui.pushButton, SIGNAL(clicked()), this, SLOT(openFile()));
 	connect(ui.pushButton_2, SIGNAL(clicked()), this, SLOT(pausePlay()));
 	connect(ui.openGLWidget, SIGNAL(resized()), this, SLOT(pictureScale()));
+	updateWidthOfPerPixel();
 }
 
 QtPlayer::~QtPlayer()
@@ -52,6 +57,17 @@ void QtPlayer::resizeEvent(QResizeEvent *event)
 	ui.widget_2->move(100, this->height() - 100);
 	ui.widget_2->resize(this->width() - 200, 100);
 	ui.openGLWidget->resize(this->size());
+	ui.openGLWidget->move(0,0);
+	mnScaleValue = 1.0;
+	mnPaperX = 0;
+	mnPaperY = 0;
+	//mnWidth = ui.openGLWidget->width();
+	//mnHeight = ui.openGLWidget->height();
+	qDebug() << "this->size(): " << this->size();
+	qDebug() << "this->width(): " << this->width() << "  this->height(): " << this->height();
+	//qDebug() << "mnWidth: " << mnWidth << "  mnHeight: " << mnHeight;
+	updateWidthOfPerPixel();
+	mbIsWheelScale = false;
 }
 
 void QtPlayer::closeEvent(QCloseEvent *event)
@@ -73,24 +89,167 @@ void QtPlayer::pictureScale()
 
 void QtPlayer::wheelEvent(QWheelEvent *event)
 {
-	mnWidth = ui.openGLWidget->width();
-	mnHeight = ui.openGLWidget->height();
-	QSize size;
-	if (event->delta() > 0) {
-		// 当滚轮远离使用者时
-		qDebug() << "滚轮事件 --------> 向前" << endl;
-		mnWidth = mnWidth + 40;
-		mnHeight = mnWidth * 9 / 16;
-		size.setWidth(mnWidth);
-		size.setHeight(mnHeight);
+	mbIsWheelScale = true;
+	QPoint numPixels = event->pixelDelta();
+	QPoint numDegrees = event->angleDelta() / 8;
+
+	if (!numPixels.isNull()) {
+		onWheelValueChanged(event->pos(), numPixels);
+	}
+	else if (!numDegrees.isNull()) {
+		QPoint numSteps = numDegrees / 15;
+		onWheelValueChanged(event->pos(), numSteps);
+	}
+	event->accept();
+}
+
+QPoint QtPlayer::mousePoint2PaperPoint(QPoint &point) {
+	QPoint ret;
+	//qDebug() << "MousePoint2PaperPoint...";
+	//qDebug() << "point.x(): " << point.x() << "  point.y(): " << point.y() << "  mnPaperX: " << mnPaperX << "  mnPaperY: " << mnPaperY;
+	ret.setX(point.x() - mnPaperX);
+	ret.setY(point.y() - mnPaperY);
+	return ret;
+}
+
+void QtPlayer::onWheelValueChanged(QPoint &mouse_pos, QPoint &step) {
+
+	//if mouse point in paper
+	if (mRect.contains(mouse_pos)) {
+		QPoint before_resize_mouse_point_at_paper = mousePoint2PaperPoint(mouse_pos);
+		qDebug()<<"before_resize_mouse_point_at_paper: " <<before_resize_mouse_point_at_paper;
+		int temp_paper_point_x = drawWidth2PaperWidth(before_resize_mouse_point_at_paper.x());
+		int temp_paper_point_y = drawWidth2PaperWidth(before_resize_mouse_point_at_paper.y());
+		qDebug()<<"temp_paper_point_x: " <<temp_paper_point_x;
+		qDebug()<<"temp_paper_point_y: " <<temp_paper_point_y;
+
+		//resize
+		int step_value = step.y();
+		mnScaleValue += static_cast<double>(step_value) / 20.0;
+		if (mnScaleValue > SCALE_VALUE_MAX) mnScaleValue = SCALE_VALUE_MAX;
+		if (mnScaleValue < SCALE_VALUE_MIN) mnScaleValue = SCALE_VALUE_MIN;
+		updateWidthOfPerPixel();
+
+		int temp_draw_point_x = paperWidth2DrawWidth(temp_paper_point_x);
+		int temp_draw_point_y = paperWidth2DrawWidth(temp_paper_point_y);
+		QPoint after_resize_mouse_point_at_paper(temp_draw_point_x, temp_draw_point_y);
+
+
+		QPoint should_move_length = after_resize_mouse_point_at_paper - before_resize_mouse_point_at_paper;
+
+		mnPaperX -= should_move_length.x();
+		mnPaperY -= should_move_length.y();
+		qDebug() << "OnWheelValueChanged1 paper point changed mnPaperX:  " << mnPaperX << "  mnPaperY: " << mnPaperY;
+
+		update();
+
+	}
+	else 
+	{ //else using center resize
+		int old_width = mRect.width();
+		int old_height = mRect.height();
+
+		//resize
+		int step_value = step.y();
+		mnScaleValue += static_cast<double>(step_value) / 20.0;
+		if (mnScaleValue > SCALE_VALUE_MAX) mnScaleValue = SCALE_VALUE_MAX;
+		if (mnScaleValue < SCALE_VALUE_MIN) mnScaleValue = SCALE_VALUE_MIN;
+		updateWidthOfPerPixel();
+
+		int new_width = paperWidth2DrawWidth(mnPaperWidth);
+		int new_height = paperWidth2DrawWidth(mnPaperHeight);
+
+		int adjusted_height = new_height - old_height;
+		int adjusted_width = new_width - old_width;
+
+		mnPaperX -= adjusted_width / 2;
+		mnPaperY -= adjusted_height / 2;
+		//qDebug()<<"OnWheelValueChanged1 paper point changed paper_x_:  " <<paper_x_<<"  paper_y_: "<<paper_y_;
+
+		update();
+	}
+}
+
+void QtPlayer::paintEvent(QPaintEvent *event)
+{
+	setRect();
+}
+
+/**
+ * update widget of per pixel
+ */
+void QtPlayer::updateWidthOfPerPixel()
+{
+	if (this->height() < this->width()) {
+		mdWidthPerPixel = calcWidthOfPerPixel(mnScaleValue, mnPaperWidth, this->width());
 	}
 	else {
-		// 当滚轮向使用者方向旋转时
-		qDebug() << "滚轮事件 --------> 向后" << endl;
-		mnWidth = mnWidth - 40;
-		mnHeight = mnWidth * 9 / 16;
-		size.setWidth(mnWidth);
-		size.setHeight(mnHeight);
+		mdWidthPerPixel = calcWidthOfPerPixel(mnScaleValue, mnPaperWidth, this->height());
 	}
-	ui.openGLWidget->resize(size);
+	qDebug() << "mdWidthPerPixel: " << mdWidthPerPixel;
 }
+
+/**
+ * calc width of per pixel
+ *
+ * @param nScaleValue: scale factor 0.5~20
+ * @param nPaperWidth: width of drawpaper 800
+ * @param nWidgetWidth: width of current openglWidget
+ * @return width of per pixel after resize
+ */
+double QtPlayer::calcWidthOfPerPixel(double &nScaleValue, int &nPaperWidth, const int &nWidgetWidth) {
+	qDebug() << "nScaleValue: " << nScaleValue
+		<< "  nPaperWidth: " << nPaperWidth 
+		<< "  nWidgetWidth: " << nWidgetWidth;
+	int nScaledWidgetWidth = qRound(static_cast<double>(nWidgetWidth) * nScaleValue);
+
+	double dWidgetOfPerPixel = 
+		static_cast<double>(nPaperWidth) /
+		static_cast<double>(nScaledWidgetWidth);
+	//limit readable per-pixel value
+	if (dWidgetOfPerPixel < 0.0005) dWidgetOfPerPixel = 0.0005;
+	//qDebug()<<"paper_width_of_per_pixel: "<< paper_width_of_per_pixel;
+	return dWidgetOfPerPixel;
+}
+
+/**
+ * set rect position and width, height.
+ * The rect for judge is if contain mouse 
+ */
+void QtPlayer::setRect()
+{
+	int nResizeWidth = paperWidth2DrawWidth(mnPaperWidth);
+	int nResizeHeight = paperWidth2DrawWidth(mnPaperHeight);
+	mRect.setX(mnPaperX);
+	mRect.setY(mnPaperY);
+	mRect.setWidth(nResizeWidth);
+	mRect.setHeight(nResizeHeight);
+
+	if (mbIsWheelScale)
+	{
+		resizeAndMoveGLWidget();
+	}
+	qDebug() << "mRect: " << mRect;
+}
+
+void QtPlayer::resizeAndMoveGLWidget()
+{
+	ui.openGLWidget->resize(mRect.width(), mRect.height());
+	ui.openGLWidget->move(mnPaperX, mnPaperY);
+}
+
+int QtPlayer::paperWidth2DrawWidth(int &paper_width)
+{
+	qDebug() << "paperWidth2DrawWidth......";
+	double draw_width = static_cast<double>(paper_width) / mdWidthPerPixel;
+	qDebug() << "draw_width:  " << draw_width << "   paper_width: " << paper_width << "   mdWidthPerPixel: " << mdWidthPerPixel;
+	return qRound(draw_width);
+}
+
+int QtPlayer::drawWidth2PaperWidth(const int &draw_width) {
+	//qDebug() << "DrawWidth2PaperWidth......";
+	double paper_width = static_cast<double>(draw_width) * mdWidthPerPixel;
+	//qDebug() << "draw_width:  " << draw_width << "   paper_width: " << paper_width << "   mdWidthPerPixel: " << mdWidthPerPixel;
+	return static_cast<int>(qRound(paper_width));
+}
+
--- QtPlayer.h
+++ QtPlayer.h
@@ -6,6 +6,7 @@
 #include <QCloseEvent>
 #include <QResizeEvent>
 #include <QWheelEvent>
+#include <QPaintEvent>
 
 class QtPlayer : public QWidget
 {
@@ -15,10 +16,21 @@ public:
 	QtPlayer(QWidget *parent = Q_NULLPTR);
 	~QtPlayer();
 
+	void updateWidthOfPerPixel();
+	double calcWidthOfPerPixel(double &scale_value,
+		int &paper_width,
+		const int &widget_width);
+	void setRect();
+	int paperWidth2DrawWidth(int &paper_width);
+	void onWheelValueChanged(QPoint &mouse_pos, QPoint &step);
+	QPoint mousePoint2PaperPoint(QPoint &point);
+	int drawWidth2PaperWidth(const int &draw_width);
+	void resizeAndMoveGLWidget();
 protected:
 	void resizeEvent(QResizeEvent *event);
 	void closeEvent(QCloseEvent *event);
 	void wheelEvent(QWheelEvent *event);
+	void paintEvent(QPaintEvent *event);
 public slots:
 	void openFile();
 	void pausePlay();
@@ -26,7 +38,22 @@ public slots:
 
 private:
 	Ui::QtPlayerClass ui;
-	XDemuxThread *demuxThread = NULL;
+
+	bool mbIsWheelScale;
+
+	const double SCALE_VALUE_MAX = 20.0;
+	const double SCALE_VALUE_MIN = 0.5;
+
+	double mdWidthPerPixel = 0.0;
+	double mnScaleValue = 1.0;
+	
 	int mnWidth = 0;
 	int mnHeight = 0;
+	int mnPaperX = 0;
+	int mnPaperY = 0;
+	int mnPaperWidth = 1000;
+	int mnPaperHeight = 1000;
+	
+	QRect mRect;
+	XDemuxThread *demuxThread = NULL;
 };
